1c5ee1346e1db1eb707d78cb60f745db06784915
Gabriel Torres <dev.gabriel.torres@gmail.com>
2023-10-21 18:46:59 -0300

    Limpando a estrutura de pastas padrão do laravel

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/', fn () => ['Projeto' => config('app.name'), 'Versão' => config('app.version')]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('/', fn () => ['Projeto' => config('app.name'), 'Versão' => '1.0.0']);
 });
